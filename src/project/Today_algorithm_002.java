11046df4dade82101070c15b784ab69ea26698cb
SHIN JIHO <jhhj424@naver.com>
2020-06-24 10:13:39 +0000

    20200624

---
+++ src/project/Today_algorithm_002.java
@@ -0,0 +1,24 @@
+/*
+문제 설명
+(세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+
+
+
+(1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+*/
+
+package project;
+
+import java.util.*;
+
+public class Today_algorithm_002 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a*(b%10));
+        System.out.println(a*((b%100)/10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+    }
+}
